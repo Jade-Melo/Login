d2aef07e3edaecf5a53ea4e09a7fe00817e2a165
Jade Melo <jade.melo@etec.sp.gov.br>
2025-06-14 00:31:37 -0300

    modificação

---
+++ teste.php
@@ -0,0 +1,20 @@
+<?php
+
+require "Usuario.class.php";
+
+$exito = $usuario = new Usuario();
+
+if($exito){
+   if(isset($_POST["cadastrar"])){
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+
+        $usuario->cadastrar($nome, $email, $senha);
+
+   }else{
+        echo "<script>
+      confirm('Erro ao conectar. Tente mais tarde')
+        <script>";
+   }     
+}
